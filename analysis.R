8f049f567cb2cc479b0ab94f2985a19febeb44f2
nperla6566-dotcom <n.perla6566@gmail.com>
2026-09-09 11:11:47 -0400

    Create analysis.R

---
+++ analysis.R
@@ -0,0 +1,10 @@
+# Sleep Duration and Cardiovascular Health
+# NHANES Data Analysis
+# Nidhi S. Perla
+
+# Load packages
+library(tidyverse)
+
+# Research question:
+# Is sleep duration associated with systolic blood pressure
+# and hypertension among U.S. adults?
